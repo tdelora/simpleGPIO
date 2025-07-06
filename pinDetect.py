264d2c467f69a9deaf20e36f49aab2236db0802f
Thomas De Lora <69022965+tdelora@users.noreply.github.com>
2025-07-05 17:29:56 -0700

    Updated comments in pinDetect.py

--- pinDetect.py
+++ pinDetect.py
@@ -7,22 +7,22 @@ GPIO.setmode(GPIO.BCM)
 # This is a basic script to trigger a Raspberry Pi GPIO pin (16).
 
 # Notes
-# Clips mean colored wires with alligator clips with a DuPont connector
+# Clips mean colored wires with an alligator clip and a DuPont connector
 #  - Amazon: https://a.co/d/iVMrlcs
-# Resitstor means resistors...
+# Resistor means resistors...
 #  - Amazon: https://a.co/d/1mahULF
 # PWR: Red clip to pin 1 (3.3v) with 330 ohm resistor
 # GRD: Black clip to pin 34 (gnd) with 1K ohm resistor
 # SWC: Blue clip to pin 16
 #
-# After starting script:
+# After starting the script:
 #  - touch SWC to PWR to trigger HIGH
 #  - Touch SWC to GRD to trigger LOW
 
 # Define the GPIO pin you want to monitor
 gpioPin = 16
 
-# Set the pin, using GPIO.PUD_DOWN in GPIO.setup should keep pin state low
+# Set the pin, using GPIO.PUD_DOWN in GPIO.setup should keep the pin state low
 # when SWC is not touching PWR.
 GPIO.setup(gpioPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
 
